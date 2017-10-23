8468a22beeae4ceafbf581b75760ca7ce6e30625
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-10-23 14:17:44 -0600

    Initial Commit, added sorting suite to node modules

--- tests/index-test.js
+++ tests/index-test.js
@@ -1,5 +1,9 @@
 import { expect } from 'chai';
-import index from '../lib/index'
+import { bubbleSort, insertionSort, mergeSort, quickSort, randomNumber } from '@hartiganhm/sorting-suite';
 
 describe('Example Test File', () => {
+	it('should successfully us sorts we imported (like badasses)', () => {
+		console.log(bubbleSort)
+		expect(bubbleSort([3, 2, 1])).to.deep.equal([1, 2, 3]);
+	})
 })
