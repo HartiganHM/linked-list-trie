18a464531c0a650dd935b1154777addcc709110c
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-10-24 14:08:49 -0600

    Wrote and passed initial Trie constructor tests

--- tests/Trie-test.js
+++ tests/Trie-test.js
@@ -8,13 +8,20 @@ let trie;
 describe('Trie', () => {
 
 	beforeEach( () => {
-		trie = new Trie('');
+		trie = new Trie();
+		node = new Node('');
 	});
 
 	it('Should be a function', () => {
 		assert.isFunction(Trie);
 	});
 
+	it('Should take a node as the root property', () => {
+		assert.deepEqual(trie.root, node);
+	});
 
+	it('Should have a default starting count of 0', () => {
+		assert.equal(trie.count, 0);
+	});
 
 })
