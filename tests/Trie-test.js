d534ade062b1d04e042f4463a05284679a326868
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-10-24 14:38:06 -0600

    Added tests to check trie.count with each insertion

--- tests/Trie-test.js
+++ tests/Trie-test.js
@@ -24,4 +24,37 @@ describe('Trie', () => {
 		assert.equal(trie.count, 0);
 	});
 
+	describe('Insert', () => {
+		it('Should have an empty starting root', () => {
+			trie.insert('pizza');
+			assert.equal(trie.root.letter, '');
+		});
+
+		it('Should have count increase when a word is inserted', () => {
+			assert.equal(trie.count, 0);
+			trie.insert('pizza');
+			assert.equal(trie.count, 1);
+		});
+
+		it('Should have count increase with mutiple words', () => {
+			assert.equal(trie.count, 0);
+			trie.insert('hog');
+			assert.equal(trie.count, 1);
+			trie.insert('branch');
+			assert.equal(trie.count, 2);
+			trie.insert('stupid');
+			assert.equal(trie.count, 3);
+		});
+
+		it('Should have a child of p from the empty root', () => {
+			trie.insert('p');
+			console.log(Object.keys(trie.root.children));
+			assert.equal(trie.root.children.letter, 'p');
+		});
+
+		it.skip('Should split a word into an array of letters', () => {
+			assert.deepEqual(trie.insert('pizza'), )
+		})
+	})
+
 })
